b14a1416fdac7b6dc103c7e15c4bbff972b24975
Xiangyu Bu <xybu92@live.com>
2016-04-11 12:53:40 -0400

    Adjust some scripts.

--- kvm/inst.sh
+++ kvm/inst.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install libglib2.0-dev
-
-# Add GetDeb repo.
-wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu trusty-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
-
-# Install packages.
-sudo apt-get update
-sudo apt-get install qemu-kvm libvirt-bin bridge-utils virt-manager virt-top virt-what ubuntu-virt
-sudo apt-get install -y virtinst
-# sudo apt-get install -y python-vm-builder
+sudo apt-get install -y libglib2.0-dev qemu-kvm libvirt-bin bridge-utils virt-manager virt-top virt-what ubuntu-virt virtinst
 
 # Create a SSH RSA key pair if missing.
 if [ ! -f ~/.ssh/id_rsa.pub ] ; then
@@ -18,6 +8,7 @@ if [ ! -f ~/.ssh/id_rsa.pub ] ; then
   ssh-keygen
 fi
 
+# sudo apt-get install -y python-vm-builder
 # Add ubuntu to sudoer
-SUDOERS_TMPL=/etc/vmbuilder/ubuntu/sudoers.tmpl
-grep -q -e 'ubuntu' $SUDOERS_TMPL || sudo sed -i '$a ubuntu ALL=(ALL) NOPASSWD:ALL\n#includedir /etc/sudoers.d' $SUDOERS_TMPL
+# SUDOERS_TMPL=/etc/vmbuilder/ubuntu/sudoers.tmpl
+# grep -q -e 'ubuntu' $SUDOERS_TMPL || sudo sed -i '$a ubuntu ALL=(ALL) NOPASSWD:ALL\n#includedir /etc/sudoers.d' $SUDOERS_TMPL
